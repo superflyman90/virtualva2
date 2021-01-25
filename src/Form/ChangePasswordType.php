c694cc007dabf39ff3f2df8ed342b959393ce585
Manfred Parbatia <superflyman90@gmail.com>
2021-01-25 22:12:46 +0100

    Mis a jour de Change Password controller + twig

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'disabled' => true,
+                'label' => "Mon adresse mail"
+            ])
+            ->add('firstname', TextType::class, [
+                'disabled' => true
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true
+            ])
+            ->add('old_password', PasswordType::class, [
+                'label' => "Mon mot de passe actuel",
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => "Veuillez saisir votre mot de passe actuel"
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => "Password and his conform has to be the same",
+                'label' => 'Mon nouveau mot de passe',
+                'required' => true,
+                'first_options' => ['label' => "Mon nouveau mot de passe"],
+                'second_options' => ['label' => "confirmez votre nouveau mot de passe"]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-3'
+                ],
+                'label' => "Mettre à jour"
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
